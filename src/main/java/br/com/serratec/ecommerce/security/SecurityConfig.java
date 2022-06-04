e06a6884e30deb62ea582371fe3957fe9059527e
biaramos7 <beatrizrmcarvalho@gmail.com>
2022-06-04 20:28:58 -0300

    criacao JWTAuthenticationFilter

--- src/main/java/br/com/serratec/ecommerce/security/SecurityConfig.java
+++ src/main/java/br/com/serratec/ecommerce/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.serratec.ecommerce.security;
 
+import br.com.serratec.ecommerce.token.JWTAuthenticationFilter;
+import br.com.serratec.ecommerce.token.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.POST;
 
@@ -22,15 +26,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
+    JWTUtil jwtUtil;
+
     private static String[]AUTH_WHITELIST = {
-        "/cliente"
         //TODO: endpoints liberados
     };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable();
-        http.authorizeRequests().antMatchers(POST, "/cliente").permitAll().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/login", "/cliente").permitAll().anyRequest().authenticated();
+        http.addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), jwtUtil),
+                UsernamePasswordAuthenticationFilter.class);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
--- src/main/java/br/com/serratec/ecommerce/token/JWTUtil.java
+++ src/main/java/br/com/serratec/ecommerce/token/JWTUtil.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Component
-public class JWUtil {
+public class JWTUtil {
 
     @Value("${jwt.secret}")
     private String secret;
