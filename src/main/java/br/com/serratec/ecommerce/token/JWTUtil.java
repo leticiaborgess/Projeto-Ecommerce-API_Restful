0eb403182dab4388ac1432f5f70da4c6394b519f
Beatriz Ramos <beatrizrmcarvalho@gmail.com>
2022-06-06 17:29:09 -0300

    criação getAuthentication

--- src/main/java/br/com/serratec/ecommerce/token/JWTUtil.java
+++ src/main/java/br/com/serratec/ecommerce/token/JWTUtil.java
@@ -3,8 +3,12 @@ package br.com.serratec.ecommerce.token;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -21,5 +25,19 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS512, secret.getBytes())
                 .compact();
+    }
+
+    public Authentication getAuthentication (HttpServletRequest request){
+        String token = request.getHeader("Authorization");
+        if(token != null){
+            String user = Jwts.parser().setSigningKey(secret.getBytes())
+                    .parseClaimsJwt(token.replace("Bearer", ""))
+                    .getBody()
+                    .getSubject();
+            if(user != null){
+                return new UsernamePasswordAuthenticationToken(user, null);
+            }
+        }
+        return null;
     }
 }
